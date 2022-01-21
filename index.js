caa2b675d34a23732bc92e59c39999c6c993435e
Ceebcheng <jonathan2075@gmail.com>
2022-01-20 19:28:41 -0600

    added stuff to index

--- index.js
+++ index.js
@@ -1,28 +1,55 @@
 const fs = require('fs');
 const inquirer = require("inquirer");
+const Manager = require('./Develop/lib/Manager');
+const Intern = require('./Develop/lib/Intern');
+const Engineer = require('./Develop/lib/Engineer');
+const generateHTML = require('./Develop/src/page-template')
 
-var test = []
-//create array for different stuff, 
-inquirer.prompt([
-    {
-        type: "input",
-        message: "What is your Team Manager's name?",
-        name: "managerName"
-    },
-    {
-        type: "input",
-        message: "What is your Team Manager's employee ID?",
-        name: "managerName"
-    },
-    {
-        type: "input",
-        message: "What is your Team Manager's name?",
-        name: "managerName"
-    },
-    {
-        type: "input",
-        message: "What is your Team Manager's name?",
-        name: "managerName"
-    },
+const squad = [];
 
-])
+const buildTeam = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "What is your Team Manager's name?",
+            name: "managerName"
+        },
+        {
+            type: "input",
+            message: "What is your Team Manager's employee ID?",
+            name: "managerID"
+        },
+        {
+            type: "input",
+            message: "What is your Team Manager's Email?",
+            name: "managerEmail"
+        },
+        {
+            type: "input",
+            message: "What is your Team Manager's Office number?",
+            name: "managerNumber"
+        },
+    
+    ])
+
+.then((answers) => {
+    console.log(answers);
+    const managerObject = new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.managerNumber);
+    squad.push(managerObject);
+    console.log(managerObject);
+    
+   
+    fs.writeFile('test.html', managerObject, (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log("Successfully created HTML");
+    })
+})
+.catch(err => {
+    console.log(err);
+})
+
+}
+
+buildTeam();
